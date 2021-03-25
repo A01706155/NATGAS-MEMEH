862fc63e6998b8adcc6b5670a9402f71a11b5068
Manolo Ramírez Pintor <A01706155@itesm.mx>
2021-03-25 16:35:40 +0000

    Mover y actualizar archivos

--- web/app.js
+++ web/app.js
@@ -1,46 +1,49 @@
-
-
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
 const path = require('path');
-
+const app = express();
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
-
-const csrf = require('csurf');
-const csrfProtection = csrf();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-app.use(bodyParser.urlencoded({extended: false}));
+const rutasUsers = require('./routes/users');
+//const rutasPersonajes = require('./routes/personajes');
 
+//Middleware
+//Para acceder fácilmente a los datos de las formas
+app.use(bodyParser.urlencoded({extended: false}));
+//Para acceder a los valores de las cookies
 app.use(cookieParser());
+//Para trabajar con sesiones
 app.use(session({
-    secret: 'ingreso', 
+    secret: 'grgrfgrgeddgaagsres', 
     resave: false, //La sesión no se guardará en cada petición, sino sólo se guardará si algo cambió 
     saveUninitialized: false, //Asegura que no se guarde una sesión para una petición que no lo necesita
 }));
 
+//Para acceder a los recursos de la carpeta public
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(csrfProtection);
 
 app.use((request, response, next) => {
     console.log('Middleware!');
-    next();
+    next(); //Le permite a la petición avanzar hacia el siguiente middleware
 });
 
-app.get('/', (request, response, next) => {
-  response.send('<h1><ul>Laboratorio 11</ul></h1>');
-    //response.sendFile(path.join(__dirname, 'index.html'));
+app.use('/users', rutasUsers);
+
+
+app.get('/inicio', (request, response, next) => {
+    response.render('inicio', { 
+    titulo: 'Inicio',
+    isLoggedIn: request.session.isLoggedIn === true ? true : false 
+    });
 });
 
 app.use((request, response, next) => {
-    //response.statusCode = 404;
-    response.status(404);
-    response.send('<h1>Page not found :(</h1>');
+    response.statusCode = 404;
+    response.send('Page not found :('); //Manda la respuesta
 });
 
-app.listen(5000);
+app.listen(3000);
