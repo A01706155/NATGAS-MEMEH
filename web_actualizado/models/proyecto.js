6c49b20c73282190920d0f56024e3e61ebf49e9e
Manolo Ramírez Pintor <A01706155@itesm.mx>
2021-04-16 03:20:21 -0500

    Proyecto db

--- web_actualizado/models/proyecto.js
+++ web_actualizado/models/proyecto.js
@@ -3,11 +3,12 @@ const db = require('../util/database');
 module.exports = class Proyecto {
 
     //Constructor de la clase. Sirve para crear un nuevo objeto, y en él se definen las propiedades del modelo
-    constructor(nombreProyecto,descripcion,fecha_inicio,fecha_fin) {
+    constructor(nombreProyecto,descripcion,fecha_inicio,fecha_fin, idProyecto) {
         this.nombreProyecto = nombreProyecto;
         this.descripcion = descripcion;
         this.fecha_inicio = fecha_inicio;
         this.fecha_fin = fecha_fin;
+        this.idProyecto = idProyecto;
     }
 
     //Este método servirá para guardar de manera persistente el nuevo objeto. 
