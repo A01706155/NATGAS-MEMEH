4b5e97bc380e75649da8ba75ed373ec08de2ffae
A01706202 <A01706202@itesm.mx>
2021-04-19 20:57:47 -0500

    vista de fases2

--- web_actualizado/controllers/fases_controller.js
+++ web_actualizado/controllers/fases_controller.js
@@ -5,20 +5,28 @@ const Fase = require('../models/fase');
 
 exports.getRegistrarFase = (request, response, next) => {
     const id = request.params.proyecto_id;
-    response.render('registrar_fase', {
-        csrfToken: request.csrfToken(),
-        titulo: 'Registrar fase nueva',
-        isLoggedIn: request.session.isLoggedIn === true ? true : false
-    });
+    Proyecto.fetchOne(id)
+        .then(([rows, fieldData]) => {
+            response.render('registrar_fase', { 
+                
+                proyecto: rows,  
+                titulo: 'Registrar fase nueva',
+                csrfToken: request.csrfToken(),
+                isLoggedIn: request.session.isLoggedIn === true ? true : false
+            });
+        })
+        .catch(err => {
+            console.log(err);
+        });
 };
 
 exports.postRegistrarFase = (request, response, next) => {
     console.log(request.body.nombreFase);
-    const nueva_fase = new Fase(request.body.nombreFase, request.body.estado);
+    const nueva_fase = new Fase(request.body.nombreFase, request.body.idProyecto);
     nueva_fase.save()
         .then(() => {
             response.setHeader('Set-Cookie', ['ultimo_proyecto='+nueva_fase.nombreFase+'; HttpOnly']);
-            response.redirect('/fases');
+            response.redirect('/fases/' + request.body.idProyecto);
         }).catch(err => console.log(err));
 
 }
@@ -29,7 +37,7 @@ exports.getProyectoFase = (request, response, next) => {
     Fase.fetchOne(id);
     console.log(id);
     //console.log(request.session.rol);
-    Proyecto.fetchOne(id)
+    Fases.fetchOne(id)
         .then(([rows, fieldData]) => {
             response.render('WBS_proyecto', { 
                 Fase: rows,
