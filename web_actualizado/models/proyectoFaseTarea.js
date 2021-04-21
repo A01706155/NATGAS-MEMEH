411a78392179f3b23973aa826e06d3053a497bfc
Manolo Ramírez Pintor <A01706155@itesm.mx>
2021-04-21 15:58:48 -0500

    Añadir modelo

---
+++ web_actualizado/models/proyectoFaseTarea.js
@@ -0,0 +1,13 @@
+const db = require('../util/database');
+
+module.exports = class Proyecto_Fase_Tarea {
+    constructor(id_proyecto, id_fase, id_tarea) {
+        this.id_proyecto = id_proyecto;
+        this.id_fase = id_fase;
+        this.id_tarea = id_tarea;
+
+    }
+
+
+
+}
