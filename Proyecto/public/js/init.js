18e5a47bb24070b4594631e588b7e36cf2cbf846
Erick Gerardo Calderon Reyes <A01351361@itesm.mx>
2021-04-26 12:11:55 -0500

    Dropdown Historias Usuario

--- Proyecto/public/js/init.js
+++ Proyecto/public/js/init.js
@@ -16,10 +16,14 @@ document.addEventListener('DOMContentLoaded', function() {
   var instances = M.Sidenav.init(elems, options);
 });
 
-// Or with jQuery
 
-$(document).ready(function(){
-  $('.sidenav').sidenav();
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.dropdown-trigger');
+  var instances = M.Dropdown.init(elems, options);
 });
 
-$(".dropdown-trigger").dropdown();
+// Or with jQuery
+
+$('.dropdown-trigger').dropdown();
