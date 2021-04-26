fef5a2ab4a2602d0dce4d034af22f8d95caa9611
Manolo Ramírez Pintor <A01706155@itesm.mx>
2021-04-26 12:18:49 -0500

    dropdown para register_casodeuso

--- Proyecto/public/js/init.js
+++ Proyecto/public/js/init.js
@@ -23,3 +23,14 @@ $(document).ready(function(){
 });
 
 $(".dropdown-trigger").dropdown();
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('select');
+  var instances = M.FormSelect.init(elems, options);
+});
+
+// Or with jQuery
+
+$(document).ready(function(){
+  $('select').formSelect();
+});
