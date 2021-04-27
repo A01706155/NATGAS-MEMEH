3527ae8a6326cd0fb29456670218c2bd57350463
A01706202 <A01706202@itesm.mx>
2021-04-27 12:11:09 -0500

    Corrección de fecha

--- Proyecto/controllers/proyectos_controller.js
+++ Proyecto/controllers/proyectos_controller.js
@@ -15,13 +15,19 @@ exports.getRegistrarProyecto = (request, response, next) => {
 
 exports.postRegistrarProyecto = (request, response, next) => {
     console.log(request.body.nombreProyecto);
-    const nuevo_proyecto = new Proyecto(request.body.nombreProyecto,request.body.descripcion,request.body.fecha_inicio,request.body.fecha_fin,request.body.estado);
-    nuevo_proyecto.save()
-        .then(() => {
-            response.setHeader('Set-Cookie', ['ultimo_proyecto='+nuevo_proyecto.nombreProyecto+'; HttpOnly']);
-            response.redirect('/proyectos');
-        }).catch(err => console.log(err));
-
+    let inicio = request.body.fecha_inicio;
+    let fin = request.body.fecha_fin;
+    if (inicio > fin) {
+        response.redirect('/proyectos/registrar-proyecto');
+    }
+    else {
+        const nuevo_proyecto = new Proyecto(request.body.nombreProyecto,request.body.descripcion,inicio,fin,request.body.estado);
+        nuevo_proyecto.save()
+            .then(() => {
+                response.setHeader('Set-Cookie', ['ultimo_proyecto='+nuevo_proyecto.nombreProyecto+'; HttpOnly']);
+                response.redirect('/proyectos');
+            }).catch(err => console.log(err));
+    }
 }
 
 exports.postBuscar = (request, response, next) => {
