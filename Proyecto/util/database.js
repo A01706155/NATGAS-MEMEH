b0bbc477988c15ee773e7dc19b3b5f0a5f6c1544
A01706202 <A01706202@itesm.mx>
2021-04-30 20:47:09 -0500

    PDF

--- Proyecto/util/database.js
+++ Proyecto/util/database.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host: 'bd-natgas.mysql.database.azure.com',
+    host: 'natgas.mysql.database.azure.com',
     user: 'natgasAdmin@bd-natgas',
     database: 'natgas_memeh',
     password: 'n4tgasPassword'
